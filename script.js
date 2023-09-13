a24479fe7ff128d384cffe6b57e445825821f98f
david31 <david31st@gmail.com>
2023-09-13 16:57:25 +0200

    animation flap

--- script.js
+++ script.js
@@ -10,22 +10,41 @@ let lastTime = 0;
 let ravens = [];
 class Raven {
     constructor() {
-        this.width = 100;
-        this.height = 50;
+        this.spriteWidth = 271;
+        this.spriteHeight = 194;
+        this.sizeModifier = Math.random() * 0.5 + 0.3;   // random between 0.4 and 0.6
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
-        this.directionX = Math.random() * 5 + 3; // random number between 3 and 8
+        this.directionX = Math.random() * 4 + 2; // random number between 1 and 5
         this.directionY = Math.random() * 5 - 2.5; // random number between -2.5 and +2.5
         this.markedForDeletion = false;
+        this.image = new Image();
+        this.image.src = 'raven.png';
+        this.frame = 0;
+        this.maxFrame = 4;
+        this.timeSinceFlap = 0;
+        this.flapInterval = Math.random() * 50 + 50;
     }
-    update() {
+    update(deltatime) {
+        // condition pour que le cobac rebondisse si il touche le haut ou le bas de la fenetre
+        if (this.y < 0 || this.y > canvas.height - this.height) {
+            this.directionY = this.directionY * -1;
+        }
         this.x -= this.directionX;
-        if (this.x < 0 - this.width) {
-            this.markedForDeletion = true;
+        this.y += this.directionY;
+        if (this.x < 0 - this.width) this.markedForDeletion = true;
+        this.timeSinceFlap += deltatime;
+        if (this.timeSinceFlap > this.flapInterval) {
+            if (this.frame > this.maxFrame) this.frame = 0;
+            else this.frame++;
+            this.timeSinceFlap = 0;
         }
     }
     draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
@@ -41,7 +60,7 @@ function animate(timestamp) {
         timeToNextRaven = 0;
         // console.log(ravens);
     };
-    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.update(deltatime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion); // vide le tableau ravens
     requestAnimationFrame(animate);
